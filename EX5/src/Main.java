e14cb4abab147727a22495dd8d73616e918e8afd
M-G-M-F <ynny730@gmail.com>
2025-11-01 10:43:25 +0200

    Update Main.java

--- EX5/src/Main.java
+++ EX5/src/Main.java
@@ -11,15 +11,7 @@ public class Main {
         arr.add(4);
         arr.add(5);
         ArrayList<Integer> result = EX5(arr);
-
-        System.out.print("[");
-        for(int i =0; i<result.size();i++)
-            if(i!=result.size()-1)
-                System.out.print(result.get(i)+",");
-            else
-                System.out.print(result.get(i));
-        System.out.print("]");
-    }
+        System.out.print(result);
     public static ArrayList<Integer> EX5(ArrayList<Integer> arr) {
         ArrayList<Integer> unique = new ArrayList<>();
         unique.add(arr.get(0));
